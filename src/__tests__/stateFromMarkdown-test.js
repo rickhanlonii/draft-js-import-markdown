d1214ea58132b1a538eabe681f6f9098e0fd3d90
Rick Hanlon II <rickhanlonii@gmail.com>
2016-07-08 14:07:59 -0400

    Add test for newline handling

--- src/__tests__/stateFromMarkdown-test.js
+++ src/__tests__/stateFromMarkdown-test.js
@@ -5,13 +5,26 @@ import stateFromMarkdown from '../stateFromMarkdown';
 import {convertToRaw} from 'draft-js';
 
 describe('stateFromMarkdown', () => {
-  let markdown = 'Hello World';
   it('should create content state', () => {
+    let markdown = 'Hello World';
     let contentState = stateFromMarkdown(markdown);
     let rawContentState = convertToRaw(contentState);
     let blocks = removeKeys(rawContentState.blocks);
     expect(blocks).toEqual(
       [{text: 'Hello World', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: []}]
+    );
+  });
+
+  it('should create content state with two blocks for two spaces & newline', () => {
+    const markdown = 'Hello  \nWorld';
+    const contentState = stateFromMarkdown(markdown);
+    const rawContentState = convertToRaw(contentState);
+    const blocks = removeKeys(rawContentState.blocks);
+    expect(blocks).toEqual(
+      [
+        {text: 'Hello', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: []},
+        {text: 'World', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: []},
+      ]
     );
   });
 });
